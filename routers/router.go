cc1f49fb6d2e07193b2a0f0caff55448f0cd83f4
meetabug <1272526724@qq.com>
2021-06-17 14:05:13 +0800

    使用 JWT 进行身份校验

--- routers/router.go
+++ routers/router.go
@@ -3,7 +3,9 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/meetabug/go-gin-example/middleware/jwt"
 	"github.com/meetabug/go-gin-example/pkg/setting"
+	"github.com/meetabug/go-gin-example/routers/api"
 	v1 "github.com/meetabug/go-gin-example/routers/api/v1"
 )
 
@@ -16,7 +18,10 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
+	r.GET("/auth", api.GetAuth)
+
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		//获取标签列表
 		apiv1.GET("/tags", v1.GetTags)
